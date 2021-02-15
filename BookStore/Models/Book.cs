fe64841b530727eb4863c30f7c33db4d4cb392cc
benjaminsierr <65034700+benjaminsierr@users.noreply.github.com>
2021-02-15 13:02:17 -0700

    context class

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
